31f61e06f4bc381a5ffc4fee087488cbeeeafada
Tavis Weeden <tavisweeden@gmail.com>
2019-10-27 18:16:06 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,7 +1,7 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {
+      :patriarch => {:name => "Lord Montague", :age => "53"
         #your key/value pairs here
         },
       :matriarch => {
