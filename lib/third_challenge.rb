2b95f738a2dd6e70bd656b12399c7baeef3f7db4
Tavis Weeden <tavisweeden@gmail.com>
2019-10-27 18:17:28 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -7,7 +7,7 @@ def third_challenge
       :matriarch => {:name => "Lady Montague", :age => "54"
         #your key/value pairs here
         },
-      :hero => {
+      :hero => {:name => "Romeo", :age => "15", :status => "alive"
         #your key/value pairs here
         },
       :hero_friends => []
